f7769fdc95b72655df546eff84e9659af6d21bad
MOHAMED ELNABARAWI <41297184+mohammednabarawy@users.noreply.github.com>
2023-03-19 14:16:29 +0300

    Update invoice_data.py

--- invoice_data.py
+++ invoice_data.py
@@ -1,10 +1,16 @@
 import createinvoice
 
 invoice_data = {
-    "invoice_number": 123,
-    "invoice_date": "02/10/2023",
-    "vendor": "example co.",
-    "customer": "example customer",
+    "seller_name": "ABC Company",
+    "seller_vat":"1234567890",
+    "seller_tax":"001",
+    "invoice_number":"INV-20220001",
+    "invoice_date":"2022-01-01",
+    "invoice_time":"12:30:00",
+    "invoice_amount":"1000.00",
+    "currency_code":"SAR",
+    "buyer_name":"XYZ Corporation",
+    "buyer_vat":"0987654321",
     "line_items": [
         {"product": "product1", "quantity": 1, "price": 20, "total": 20}
     ]
