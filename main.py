fd5469d222a513de070e7d4db53161e59e91b8e0
mohammednabarawy@gmail.com <mohammednabarawy@gmail.com>
2023-03-24 19:31:29 +0300

    invoice xml

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
 import json
 import base64
-import requests
-import datetime
 import xml.etree.ElementTree as ET
 from OpenSSL import crypto
 import hashlib
 from PIL import Image
 import qrcode
-from genkeys import *
-from getcsid import *
-from cryptography.hazmat.primitives import serialization, hashes
-from cryptography.hazmat.primitives.asymmetric import padding
+from genkeys import generatekeys
+from getcsid import get_csid
 
 generatekeys()
 get_csid()
@@ -26,11 +22,12 @@ invoice_data = {
     "invoice_date": "2022-01-01",
     "invoice_time": "12:30:00",
     "invoice_amount": "1000.00",
+    "tax_amount": "1150.00",
     "currency_code": "SAR",
     "buyer_name": "XYZ Corporation",
     "buyer_vat": "310864207200003",
     "line_items": [
-        {"product": "product1", "quantity": 1, "price": 20, "total": 20}
+        {"product": "product1", "quantity": 1, "price": 1000, "total": 1000}
     ]
 }
 
@@ -42,13 +39,7 @@ class SaudiEInvoice:
     def get_keys(self):
         with open(self.config_file_path) as f:
             config = json.load(f)
-
-        username = config['username']
-        password = config['password']
-        private_key_path = config['private_key_path']
-        url = config['url']
-
-        return username, password, private_key_path, url
+        return config['username'], config['password'], config['private_key_path'], config['url']
 
     def create_invoice(self, invoice_data):
         private_key_path = self.get_keys()[2]
@@ -57,130 +48,83 @@ class SaudiEInvoice:
 
         pkey = crypto.load_privatekey(crypto.FILETYPE_PEM, private_key)
 
-        # Create root element for invoice
-        root = ET.Element("{urn:oasis:names:specification:ubl:schema:xsd:Invoice-2}Invoice",
-                          xmlns="urn:oasis:names:specification:ubl:schema:xsd:Invoice-2")
-
-        # Add invoice header data
-        invoice_header = ET.SubElement(
-            root, "{urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2}InvoiceHeader")
-        invoice_id = ET.SubElement(
-            invoice_header, "{urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2}ID")
-        invoice_id.text = invoice_data["invoice_number"]
-        invoice_issue_date = ET.SubElement(
-            invoice_header, "{urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2}IssueDate")
-        invoice_issue_date.text = datetime.datetime.now().strftime('%Y-%m-%d')
-        invoice_type_code = ET.SubElement(
-            invoice_header, "{urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2}InvoiceTypeCode")
-        invoice_type_code.text = '400'
-
-        # Add seller data
-        seller_party = ET.SubElement(
-            invoice_header, "{urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2}AccountingSupplierParty")
-        seller_party_id = ET.SubElement(
-            seller_party, "{urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2}PartyIdentification")
-        seller_party_id_text = ET.SubElement(
-            seller_party_id, "{urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2}ID")
-        seller_party_id_text.text = invoice_data["seller_vat"]
-        seller_party_name = ET.SubElement(
-            seller_party, "{urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2}PartyName")
-        seller_party_name_text = ET.SubElement(
-            seller_party_name, "{urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2}Name")
-        seller_party_name_text.text = invoice_data["seller_name"]
-        seller_party_address = ET.SubElement(
-            seller_party, "{urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2}PostalAddress")
-        seller_party_street = ET.SubElement(
-            seller_party_address, "{urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2}StreetName")
-        seller_party_street.text = invoice_data["seller_street"]
-        seller_party_building = ET.SubElement(
-            seller_party_address, "{urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2}BuildingNumber")
-        seller_party_building.text = invoice_data["seller_building"]
-        seller_party_postal_zone = ET.SubElement(
-            seller_party_address, "{urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2}postal_zone")
-        seller_party_postal_zone.text = invoice_data["postal_zone"]
-        seller_address_city_name = ET.SubElement(
-            seller_party_address, "{urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2}CityName")
-        seller_address_city_name.text = invoice_data["seller_city_name"]
-
-        # Add invoice lines
-        invoice_total = 0
-        invoice_lines = ET.SubElement(root, "InvoiceLine")
-        for item in invoice_data["line_items"]:
-            item_elem = ET.SubElement(invoice_lines, "InvoiceLine")
-            item_quantity = ET.SubElement(item_elem, "InvoicedQuantity")
-            item_quantity.set("unitCode", "C62")
-            item_quantity.text = str(item["quantity"])
-            item_price = ET.SubElement(item_elem, "Price")
-            item_price.set("currencyID", invoice_data["currency_code"])
-            ET.SubElement(item_price, "PriceAmount",
-                          currencyID=invoice_data["currency_code"]).text = str(item["price"])
-            item_total = ET.SubElement(item_elem, "LineExtensionAmount",
-                                       currencyID=invoice_data["currency_code"])
-            item_total.text = str(item["total"])
-            invoice_total += item["total"]
-
-        # Add invoice summary data
-        invoice_summary = ET.SubElement(root, "InvoiceSummary")
-        ET.SubElement(invoice_summary, "TaxableAmount").text = str(
-            invoice_total)
-        ET.SubElement(invoice_summary, "TaxAmount").text = str(
-            invoice_total * 0.15)  # assuming a 15% VAT rate
-        ET.SubElement(invoice_summary, "InvoiceTotal").text = str(
-            invoice_total + (invoice_total * 0.15))
-
-        # Calculate the invoice hash
-        invoice_hash = hashlib.sha256(
-            ET.tostring(root, encoding='utf-8')).hexdigest()
-        with open('invoicehash.txt', 'w') as f:
-            f.write(invoice_hash)
-        # Sign the invoice hash
-        signature = crypto.sign(pkey, invoice_hash, "sha256")
-
-        # Load the certificate
-        try:
-            with open('certificate.txt', "rb") as cert_file:
-                cert = cert_file.read()
-                # Get the certificate hash
-                cert_hash = hashlib.sha256(cert_file.read()).hexdigest()
-        except Exception as e:
-            print(str(e))
+        path = 'Simplified_Invoice.xml'  # Replace with actual path to invoice template file
+        tree = ET.parse(path)
+        root = tree.getroot()
 
         # Add QR code to invoice
         qr_data = f"||1|1|{invoice_data['seller_name']}|{invoice_data['seller_vat']}|{invoice_data['invoice_number']}|{invoice_data['invoice_date']}|{invoice_data['invoice_time']}|{invoice_data['invoice_amount']}|{invoice_data['currency_code']}|{invoice_data['buyer_name']}|{invoice_data['buyer_vat']}"
         qr_code = base64.b64encode(qr_data.encode()).decode()
-        ET.SubElement(root, "QRCode").text = qr_code
 
-        # Add ZATCA metadata to invoice
-        zatca_data = ET.SubElement(
-            root, "{urn:zatca:metadata:schema:xsd:ZATCA_metadata-1}ZATCA_metadata")
-        ET.SubElement(
-            zatca_data, "{urn:zatca:metadata:schema:xsd:ZATCA_metadata-1}CertificateHash").text = cert_hash
-        ET.SubElement(
-            zatca_data, "{urn:zatca:metadata:schema:xsd:ZATCA_metadata-1}InvoiceHash").text = invoice_hash
+        # Find the relevant elements to replace
+        ns = {'cbc': 'urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2'}
+        seller_name = root.find('.//cbc:RegistrationName', ns)
+        seller_vat = root.find('.//cbc:CompanyID', ns)
+        seller_street = root.find('.//cbc:StreetName', ns)
+        seller_building = root.find('.//cbc:BuildingNumber', ns)
+        postal_zone = root.find('.//cbc:PostalZone', ns)
+        seller_city_name = root.find('.//cbc:CityName', ns)
+        invoice_number = root.find('.//cbc:ID', ns)
+        invoice_date = root.find('.//cbc:IssueDate', ns)
+        invoice_time = root.find('.//cbc:IssueTime', ns)
+        invoice_amount = root.find('.//cbc:PayableAmount', ns)
+        currency_code = root.find('.//cbc:DocumentCurrencyCode', ns)
+        # buyer_name = root.find('.//cbc:BuyerReference', ns)
+        # buyer_vat = root.find('.//cbc:CustomerAssignedAccountID', ns)
+        # line_items = root.find('LineItems')
+        X509Certificate = root.find('.//cbc:X509Certificate', ns)
+        SigningTime = root.find('.//cbc:IssueDate', ns)
+        signing_cert = root.find(
+            './/ds:DigestValue', {'ds': 'http://www.w3.org/2000/09/xmldsig#'})
+        X509SerialNumber = root.find(
+            './/ds:X509SerialNumber', {'ds': 'http://www.w3.org/2000/09/xmldsig#'})
+        ID = root.find('.//cbc:ID', ns)
+        UUID = root.find('.//cbc:UUID', ns)
+        QR = root.find('.//cbc:EmbeddedDocumentBinaryObject', ns)
+        # Replace the data with your own values
 
-        # Add ZATCA signature to invoice
+        seller_name.text = invoice_data["seller_name"]
+        seller_vat.text = invoice_data["seller_vat"]
+        seller_street.text = invoice_data["seller_street"]
+        seller_building.text = invoice_data["seller_building"]
+        postal_zone.text = invoice_data["postal_zone"]
+        seller_city_name.text = invoice_data["seller_city_name"]
+        invoice_number.text = invoice_data["invoice_number"]
+        invoice_date.text = invoice_data["invoice_date"]
+        invoice_time.text = invoice_data["invoice_time"]
+        invoice_amount.text = invoice_data["invoice_amount"]
+        currency_code.text = invoice_data["currency_code"]
+        QR.text = qr_code
+        # buyer_name.text = invoice_data["invoice_number"]
+        # buyer_vat.text = invoice_data["invoice_number"]
+        # Replace the line items
+        line_items = root.find('LineItems', ns)
+        if line_items is not None:
+            for item in invoice_data['line_items']:
+                line_item = ET.Element('LineItem')
+                item_description = ET.SubElement(line_item, 'ItemDescription')
+                item_description.text = item['product']
+                quantity = ET.SubElement(line_item, 'Quantity')
+                quantity.text = str(item['quantity'])
+                unit_price = ET.SubElement(line_item, 'UnitPrice')
+                unit_price.text = str(item['price'])
+                line_items.append(line_item)
 
-        signed_properties = self.create_signed_properties(pkey)
+        # Calculate the invoice hash and sign it
+        invoice_hash = hashlib.sha256(
+            ET.tostring(root, encoding='utf-8')).hexdigest()
+        signature = crypto.sign(pkey, invoice_hash, "sha256")
 
-        zatca_signature_elem = ET.SubElement(
-            root, "{urn:zatca:metadata:schema:xsd:ZATCA_metadata-1}ZATCA_signature")
-        ET.SubElement(zatca_signature_elem,
-                      "{urn:zatca:metadata:schema:xsd:ZATCA_metadata-1}SignedProperties").text = signed_properties
-        zatca_signature_value_elem = ET.SubElement(
-            zatca_signature_elem, "{urn:zatca:metadata:schema:xsd:ZATCA_metadata-1}SignatureValue")
-        zatca_signature_value = crypto.sign(
-            pkey, signed_properties.encode(), "sha256")
-        zatca_signature_value_elem.text = base64.b64encode(
-            zatca_signature_value).decode()
-
-        # Sign the final invoice with ZATCA signature
+        # Load the certificate, get its hash, and sign the final invoice with ZATCA signature
+        with open('certificate.txt', "rb") as cert_file:
+            cert = cert_file.read()
+            cert_hash = hashlib.sha256(cert_file.read()).hexdigest()
+        zatca_signature = crypto.sign(
+            pkey, ET.tostring(root, encoding='utf-8'), "sha256")
         zatca_signed_data = ET.tostring(root, encoding='utf-8')
-        zatca_signature = crypto.sign(pkey, zatca_signed_data, "sha256")
         signed_zatca_invoice_data = zatca_signed_data + zatca_signature
-
         # Write the signed invoice to a file
-        with open("invoice.xml", "wb") as output_file:
-            output_file.write(zatca_signed_data)
+        tree.write('invoice.xml')
 
         # Encode the signed ZATCA invoice data in base64
         encoded_zatca_invoice_data = base64.b64encode(
